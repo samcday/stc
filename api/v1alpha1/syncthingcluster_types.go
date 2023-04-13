9b6618d46d993de2c1a26d95090e2a7ed8574909
Sam Day <me@samcday.com>
2023-04-14 00:07:36 +0200

    Basic DaemonSet reconciliation

--- api/v1alpha1/syncthingcluster_types.go
+++ api/v1alpha1/syncthingcluster_types.go
@@ -17,31 +17,27 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// SyncthingClusterSpec defines the desired state of SyncthingCluster
 type SyncthingClusterSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of SyncthingCluster. Edit syncthingcluster_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	PodSpec corev1.PodSpec `json:"podSpec,omitempty"`
 }
 
-// SyncthingClusterStatus defines the observed state of SyncthingCluster
+type SyncthingClusterStatusNode struct {
+	DeviceID string `json:"deviceID,omitempty"`
+	Version  string `json:"version,omitempty"`
+}
+
 type SyncthingClusterStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Conditions []metav1.Condition                    `json:"conditions,omitempty"`
+	Nodes      map[string]SyncthingClusterStatusNode `json:"nodes,omitempty"`
 }
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 
-// SyncthingCluster is the Schema for the syncthingclusters API
 type SyncthingCluster struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -52,7 +48,6 @@ type SyncthingCluster struct {
 
 //+kubebuilder:object:root=true
 
-// SyncthingClusterList contains a list of SyncthingCluster
 type SyncthingClusterList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
--- controllers/syncthingcluster_controller.go
+++ controllers/syncthingcluster_controller.go
@@ -18,45 +18,304 @@ package controllers
 
 import (
 	"context"
-
+	"encoding/json"
+	"fmt"
+	stcsamcdaycomv1alpha1 "github.com/samcday/stc/api/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"net/http"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	stcsamcdaycomv1alpha1 "github.com/samcday/stc/api/v1alpha1"
 )
 
-// SyncthingClusterReconciler reconciles a SyncthingCluster object
 type SyncthingClusterReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
 
+type SyncthingRestTransport struct {
+	http.RoundTripper
+}
+
+func (srt *SyncthingRestTransport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+	req.Header.Set("X-API-Key", "boobies")
+	return srt.RoundTripper.RoundTrip(req)
+}
+
+const finalizerName = "stc.samcday.com/finalizer"
+
 //+kubebuilder:rbac:groups=stc.samcday.com,resources=syncthingclusters,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=stc.samcday.com,resources=syncthingclusters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=stc.samcday.com,resources=syncthingclusters/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the SyncthingCluster object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.1/pkg/reconcile
+//+kubebuilder:rbac:groups=apps,resources=daemonsets,verbs=get;list;watch;create;update;patch;delete
+
 func (r *SyncthingClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx).WithValues("name", req.Name)
 
-	// TODO(user): your logic here
+	var c stcsamcdaycomv1alpha1.SyncthingCluster
+	err := r.Client.Get(ctx, req.NamespacedName, &c)
+	if err != nil {
+		log.Error(err, "failed to get SyncthingCluster")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
-	return ctrl.Result{}, nil
+	hasFinalizer := controllerutil.ContainsFinalizer(&c, finalizerName)
+	isDeleted := !c.ObjectMeta.DeletionTimestamp.IsZero()
+
+	if isDeleted && !hasFinalizer {
+		// This object is in the process of being deleted, and does not have our finalizer.
+		// Nothing to do here.
+		return ctrl.Result{}, nil
+	}
+
+	var ds appsv1.DaemonSet
+	err = r.Client.Get(ctx, req.NamespacedName, &ds)
+	if err != nil && !errors.IsNotFound(err) {
+		return ctrl.Result{}, err
+	}
+
+	dsPatch := client.MergeFrom(ds.DeepCopy())
+	dsExists := ds.Name != ""
+
+	owner := metav1.GetControllerOf(&ds)
+	if dsExists && (owner == nil || owner.APIVersion != stcsamcdaycomv1alpha1.GroupVersion.String() || owner.Kind != c.Kind) {
+		// TODO: handle this properly.
+		return ctrl.Result{}, fmt.Errorf("an unmanaged DaemonSet already exists")
+	}
+
+	ds.Name = req.Name
+	ds.Namespace = req.Namespace
+	r.ensureDaemonSet(&ds, &c)
+
+	if dsExists {
+		log.V(1).Info("Patched DaemonSet for SyncthingCluster", "name", req.Name)
+		err = r.Patch(ctx, &ds, dsPatch)
+	} else {
+		log.V(1).Info("Created DaemonSet for SyncthingCluster", "name", req.Name)
+		if err := ctrl.SetControllerReference(&c, &ds, r.Scheme); err != nil {
+			return ctrl.Result{}, err
+		}
+		err = r.Create(ctx, &ds)
+	}
+
+	log.V(1).Info("DaemonSet reconciled", "generation", ds.Generation)
+
+	dss := ds.Status
+	if dss.ObservedGeneration < ds.Generation || dss.NumberReady < dss.DesiredNumberScheduled || dss.UpdatedNumberScheduled < dss.DesiredNumberScheduled {
+		log.V(1).Info("DaemonSet not yet up to date")
+		return ctrl.Result{}, r.updateReadiness(ctx, &c, false, "DaemonSetNotUpToDate", "DaemonSet has not fully rolled out yet")
+	}
+
+	if !hasFinalizer {
+		// Cluster is not being deleted, but does not yet have finalizer. Add it now.
+		controllerutil.AddFinalizer(&c, finalizerName)
+		if err := r.Update(ctx, &c); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Enumerate the created Syncthing pods, ping the REST status API of each.
+	var pods corev1.PodList
+	err = r.List(ctx, &pods, client.InNamespace(req.Namespace), client.MatchingFields{"daemonset-owner": ds.Name})
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	cli := &http.Client{
+		Transport: &SyncthingRestTransport{RoundTripper: http.DefaultTransport},
+	}
+	c.Status.Nodes = make(map[string]stcsamcdaycomv1alpha1.SyncthingClusterStatusNode)
+	for _, pod := range pods.Items {
+		var versionResp struct {
+			Version string `json:"version,omitempty"`
+		}
+		var statusResp struct {
+			ID string `json:"myID,omitempty"`
+		}
+
+		resp, err := cli.Get(fmt.Sprintf("http://%s:8384/rest/system/version", pod.Status.PodIP))
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		err = json.NewDecoder(resp.Body).Decode(&versionResp)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		resp, err = cli.Get(fmt.Sprintf("http://%s:8384/rest/system/status", pod.Status.PodIP))
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		err = json.NewDecoder(resp.Body).Decode(&statusResp)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		c.Status.Nodes[pod.Spec.NodeName] = stcsamcdaycomv1alpha1.SyncthingClusterStatusNode{
+			DeviceID: statusResp.ID,
+			Version:  versionResp.Version,
+		}
+	}
+
+	err = r.Status().Update(ctx, &c)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// All ready pods have been successfully pinged via REST API.
+	//isReady := false
+
+	if isDeleted {
+		// Check that Syncthing cluster is empty before removing the finalizer.
+		return ctrl.Result{}, fmt.Errorf("not implemented yet")
+	}
+
+	return ctrl.Result{}, r.updateReadiness(ctx, &c, true, "InSync", "TODO")
 }
 
-// SetupWithManager sets up the controller with the Manager.
+func (r *SyncthingClusterReconciler) ensureDaemonSet(ds *appsv1.DaemonSet, c *stcsamcdaycomv1alpha1.SyncthingCluster) {
+	appLabels := map[string]string{
+		"app.kubernetes.io/name":      "stc",
+		"app.kubernetes.io/instance":  ds.Name,
+		"app.kubernetes.io/component": "syncthing",
+	}
+
+	ds.Spec.Selector = &metav1.LabelSelector{
+		MatchLabels: appLabels,
+	}
+	dsTmpl := &ds.Spec.Template
+	dsTmpl.Labels = appLabels
+	dsTmpl.Spec = *c.Spec.PodSpec.DeepCopy()
+	podSpec := &dsTmpl.Spec
+	podSpec.RestartPolicy = corev1.RestartPolicyAlways
+
+	if podSpec.Affinity == nil {
+		podSpec.Affinity = &corev1.Affinity{}
+	}
+	aff := podSpec.Affinity
+
+	if aff.NodeAffinity == nil {
+		aff.NodeAffinity = &corev1.NodeAffinity{}
+	}
+	nodeAff := aff.NodeAffinity
+	if nodeAff.RequiredDuringSchedulingIgnoredDuringExecution == nil {
+		nodeAff.RequiredDuringSchedulingIgnoredDuringExecution = &corev1.NodeSelector{}
+	}
+
+	// Ensure node affinity includes support Syncthing platforms (amd64, arm64, arm)
+	rdside := nodeAff.RequiredDuringSchedulingIgnoredDuringExecution
+	rdside.NodeSelectorTerms = append(rdside.NodeSelectorTerms, corev1.NodeSelectorTerm{
+		MatchExpressions: []corev1.NodeSelectorRequirement{
+			{
+				Key:      "kubernetes.io/arch",
+				Operator: "In",
+				Values:   []string{"amd64", "arm64", "arm"},
+			},
+		},
+	})
+
+	// Ensure pod has a "syncthing-data" volume, which is a hostPath mount.
+	var vol *corev1.Volume
+	for i := range podSpec.Volumes {
+		if podSpec.Volumes[i].Name == "syncthing-data" {
+			vol = &podSpec.Volumes[i]
+			break
+		}
+	}
+	if vol == nil {
+		podSpec.Volumes = append(podSpec.Volumes, corev1.Volume{})
+		vol = &podSpec.Volumes[len(podSpec.Volumes)-1]
+	}
+	hostPath := ""
+	if vol.VolumeSource.HostPath != nil {
+		hostPath = vol.VolumeSource.HostPath.Path
+	}
+	if hostPath == "" {
+		hostPath = "/var/syncthing/" + string(c.UID)
+	}
+	hostPathType := corev1.HostPathDirectoryOrCreate
+	*vol = corev1.Volume{
+		Name: "syncthing-data",
+		VolumeSource: corev1.VolumeSource{
+			HostPath: &corev1.HostPathVolumeSource{
+				Type: &hostPathType,
+				Path: hostPath,
+			},
+		},
+	}
+
+	// Ensure pod has a "syncthing" container.
+	var ctr *corev1.Container
+	for i := range podSpec.Containers {
+		if podSpec.Containers[i].Name == "syncthing" {
+			ctr = &podSpec.Containers[i]
+			break
+		}
+	}
+	if ctr == nil {
+		podSpec.Containers = append(podSpec.Containers, corev1.Container{
+			Name: "syncthing",
+		})
+		ctr = &podSpec.Containers[len(podSpec.Containers)-1]
+	}
+
+	if ctr.Image == "" {
+		ctr.Image = "syncthing/syncthing:1"
+	}
+
+	// Ensure syncthing container has a /var/syncthing volume mount referencing syncthing-data volume.
+	var mnt *corev1.VolumeMount
+	for i := range ctr.VolumeMounts {
+		if ctr.VolumeMounts[i].MountPath == "/var/syncthing" {
+			mnt = &ctr.VolumeMounts[i]
+			break
+		}
+	}
+	if mnt == nil {
+		ctr.VolumeMounts = append(ctr.VolumeMounts, corev1.VolumeMount{
+			MountPath: "/var/syncthing",
+		})
+		mnt = &ctr.VolumeMounts[len(ctr.VolumeMounts)-1]
+	}
+	mnt.Name = "syncthing-data"
+}
+
+func (r *SyncthingClusterReconciler) updateReadiness(ctx context.Context, c *stcsamcdaycomv1alpha1.SyncthingCluster, isReady bool, reason, message string) error {
+	readyStatus := metav1.ConditionFalse
+	if isReady {
+		readyStatus = metav1.ConditionTrue
+	}
+	meta.SetStatusCondition(&c.Status.Conditions, metav1.Condition{
+		Type:    "Ready",
+		Status:  readyStatus,
+		Reason:  reason,
+		Message: message,
+	})
+
+	return r.Status().Update(ctx, c)
+}
+
 func (r *SyncthingClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	err := mgr.GetFieldIndexer().IndexField(context.Background(), &corev1.Pod{}, "daemonset-owner", func(o client.Object) []string {
+		pod := o.(*corev1.Pod)
+		owner := metav1.GetControllerOf(pod)
+		if owner == nil || owner.APIVersion != "apps/v1" || owner.Kind != "DaemonSet" {
+			return nil
+		}
+		return []string{owner.Name}
+	})
+	if err != nil {
+		return err
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&stcsamcdaycomv1alpha1.SyncthingCluster{}).
+		Owns(&appsv1.DaemonSet{}).
 		Complete(r)
 }
